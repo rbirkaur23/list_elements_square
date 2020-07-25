46128a15689141117838d501b11de5329a4e4d2a
rbirkaur23 <64214931+rbirkaur23@users.noreply.github.com>
2020-07-25 22:35:24 +0530

    Created squarelist.py

---
+++ squarelist.py
@@ -0,0 +1,6 @@
+x=[]
+n=int(input("Enter number of elements: "))
+for i in range(0,n):
+    p=int(input("Enter number: "))
+    x.append(p**2)
+print("List containing squares of elements entered:",x)
